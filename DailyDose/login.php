39505dbf794a2fcede37e8f68e36d5446448244f
Lilli Jimenez <32343231+LMarlette@users.noreply.github.com>
2018-09-02 08:10:52 -0400

    Add files via upload

---
+++ DailyDose/login.php
@@ -0,0 +1,31 @@
+
+<?php
+$con=mysqli_connect("35.203.71.49", "dailydose", "lilli","root");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+
+
+
+
+
+/*cloudsql_connection_name: skilled-orbit-215119:northamerica-northeast1:dailydose
+cloudsql_database_name: dailydose
+cloudsql_user: lilli
+cloudsql_password: root
+cloudsql_port: 3306*/
+
+
+
+?>
+
+<html>
+
+<head>
+<link rel="icon" type="image/png" href="images/logo.png"/>
+
+</head>
+</html>
